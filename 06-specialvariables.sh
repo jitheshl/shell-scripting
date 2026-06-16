cbf86952524b24730f85bf7d4c5948e4e1d7efe5
unknown <jitheshlakkala2007.com>
2026-06-16 13:37:12 +0530

    array indexing

--- 06-specialvariables.sh
+++ 06-specialvariables.sh
@@ -5,5 +5,6 @@ echo "script name:- $0"
 echo "present working directory:- $PWD"
 echo "home directory:- $HOME"
 echo "process id of current script:- $$"
+echo "user name:- $USER" 
 sleep 20 &
 echo "process id of last command background:- $!"
