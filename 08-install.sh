5c4be2187051a09971352bed6cc47a99dd3a8790
unknown <jitheshlakkala2007.com>
2026-06-17 13:24:31 +0530

    installing using shell-scripting

--- 08-install.sh
+++ 08-install.sh
@@ -3,7 +3,7 @@ userid=$(id -u)
 if [ $userid -ne 0 ]
 then
     echo "Error :: it should be in root user"
-   # exit 1
+    exit 1
 fi
 dnf install mysql -y
 if [ $? -ne 0 ]
@@ -14,4 +14,11 @@ else
 echo "installing.... mysql sucessful"
 fi
 sudo dnf install git -y
+if [ $? -ne 0 ]
+then
+echo "installing.... mysql failure"
+exit 1
+else
+echo "installing.... mysql sucessful"
+fi
 
