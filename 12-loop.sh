fff19665cf476fd59b23a5c6cede9d863c37d091
unknown <jitheshlakkala2007.com>
2026-06-17 21:37:58 +0530

    loops in install

--- 12-loop.sh
+++ 12-loop.sh
@@ -1,6 +1,49 @@
 #!/bin/bash
 
-for i in {1..100}
-do 
-echo $i
+# for i in {1..100}
+# do 
+# echo $i
+# done
+
+#!/bin/bash
+userid=$(id -u)
+r="\e[31m"
+g="\e[32m"
+y="\e[33m"
+
+log_folder="/var/log/shellscript-log"
+log_files=$(echo $0 | cut -d "." -f1)
+time_stamp=$(date +%d-%m-%Y-%H-%M-%S)
+log_file_name="$log_folder/$log_files-$time_stamp.log"
+if [ $userid -ne 0 ]
+then
+    echo "Error :: it should be in root user"
+    exit 1
+fi
+validate(){
+    if [ $1 -ne 0 ]
+    then
+    echo -e "$r Ainstalling.... $2 failure"
+    exit 1
+    else
+    echo "$g installing.... $2 sucessful"
+    fi
+}
+
+for packages in $@
+do
+dnf list installed $package &>>log_file_name
+if [ $? -ne 0 ]
+then
+    dnf install $package -y
+    validate $? $package
+    
+else
+echo -e "$y $package is already installed"
+fi
+
 done
+
+
+echo "Script started executing at : $time_stamp" &>>$log_file_name
+
